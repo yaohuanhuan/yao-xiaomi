6cbf6e96b7641d46642c094f803cae195bac4576
yaohuanhuan <yaohuanhuan@great-tao.com>
2018-08-13 17:22:00 +0800

    测试组件

---
+++ src/components/Arrows.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    arrows
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Arrows',
+    data() {
+      return {}
+    },
+    created() {
+      this.fetchData()
+    },
+    watch: {
+      '': 'fetchData'
+    },
+    methods: {
+      fetchData() {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+
+  .test {
+
+  }
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    home123
+    <Arrows></Arrows>
+  </div>
+</template>
+
+<script>
+
+  import Arrows from '../components/Arrows'
+
+  export default {
+    name: 'Home',
+    components: {
+      Arrows
+    },
+    data() {
+      return {}
+    },
+    created() {
+      this.fetchData()
+    },
+    watch: {
+      '': 'fetchData'
+    },
+    methods: {
+      fetchData() {
+        console.log(this.$router)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .test {
+
+  }
+
+</style>
